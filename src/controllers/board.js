9d9815037be50a23fb206a34d9f7d0040100e33d
Leuchtkafer <merylins5@yandex.ru>
2019-09-20 18:14:50 +0300

    Реализация переключения экранов (задачи/статистика), поиска и добавления новых задач

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -1,54 +1,93 @@
 import {Board} from '../components/board';
 import {Sort} from '../components/sort';
 import {TaskList} from '../components/tasks-list';
-import {TaskController} from '../controllers/task';
+import {TaskListController} from '../controllers/task-list';
+import {ButtonMore} from '../components/button-more';
 import {unrender, render, Position} from '../utils';
 
+const TASKS_IN_ROW = 4;
+
 export class BoardController {
-  constructor(container, tasks) {
+  constructor(container, onDataChange) {
     this._container = container;
-    this._tasks = tasks;
+    this._onDataChangeMain = onDataChange;
+    this._tasks = [];
+
+    this._showedTasks = TASKS_IN_ROW;
+
     this._board = new Board();
+    this._loadMoreButton = new ButtonMore();
     this._sort = new Sort();
     this._taskList = new TaskList();
 
-    this._subscriptions = [];
-    this._onChangeView = this._onChangeView.bind(this);
-    this._onDataChange = this._onDataChange.bind(this);
+    this._taskListController = new TaskListController(this._taskList.getElement(), this._onDataChange.bind(this));
+
+    this._init();
   }
 
-  init() {
+  hide() {
+    this._board.getElement().classList.add(`visually-hidden`);
+  }
+
+  show(tasks) {
+    if (tasks !== this._tasks) {
+      this._setTasks(tasks);
+    }
+
+    this._board.getElement().classList.remove(`visually-hidden`);
+  }
+
+  createTask() {
+    this._taskListController.createTask();
+  }
+
+  _init() {
     render(this._container, this._board.getElement(), Position.BEFOREEND);
     render(this._board.getElement(), this._sort.getElement(), Position.AFTERBEGIN);
     render(this._board.getElement(), this._taskList.getElement(), Position.BEFOREEND);
-
-    this._tasks.forEach((taskMock) => this._renderTask(taskMock));
 
     this._sort.getElement()
     .addEventListener(`click`, (evt) => this._onSortLinkClick(evt));
   }
 
   _renderBoard() {
-    unrender(this._taskList.getElement());
-
-    this._taskList.removeElement();
     render(this._board.getElement(), this._taskList.getElement(), Position.BEFOREEND);
-    this._tasks.forEach((taskMock) => this._renderTask(taskMock));
+
+    unrender(this._loadMoreButton.getElement());
+    this._loadMoreButton.removeElement();
+    if (this._showedTasks < this._tasks.length) {
+      render(this._board.getElement(), this._loadMoreButton.getElement(), Position.BEFOREEND);
+    }
+
+    this._taskListController.setTasks(this._tasks.slice(0, this._showedTasks));
+
+    this._loadMoreButton.getElement()
+    .addEventListener(`click`, () => this._onLoadMoreButtonClick());
   }
 
-  _onChangeView() {
-    this._subscriptions.forEach((it) => it());
+  _setTasks(tasks) {
+    this._tasks = tasks;
+    this._showedTasks = TASKS_IN_ROW;
+
+    this._renderBoard();
   }
 
-  _renderTask(task) {
-    const taskController = new TaskController(this._taskList, task, this._onChangeView, this._onDataChange);
-    this._subscriptions.push(taskController.setDefaultView.bind(taskController));
+  _onDataChange(tasks) {
+    this._tasks = tasks;
+    this._onDataChangeMain(this._tasks);
 
+    this._renderBoard();
   }
 
-  _onDataChange(newData, oldData) {
-    this._tasks[this._tasks.findIndex((it2) => it2 === oldData)] = newData;
-    this._renderBoard(this._tasks);
+  _onLoadMoreButtonClick() {
+    this._taskListController.addTasks(this._tasks.slice(this._showedTasks, this._showedTasks + TASKS_IN_ROW));
+
+    this._showedTasks += TASKS_IN_ROW;
+
+    if (this._showedTasks >= this._tasks.length) {
+      unrender(this._loadMoreButton.getElement());
+      this._loadMoreButton.removeElement();
+    }
   }
 
   _onSortLinkClick(evt) {
@@ -58,19 +97,17 @@ export class BoardController {
       return;
     }
 
-    this._taskList.getElement().innerHTML = ``;
-
     switch (evt.target.dataset.sortType) {
       case `date-up`:
         const sortedByDateUpTasks = this._tasks.slice().sort((a, b) => a.dueDate - b.dueDate);
-        sortedByDateUpTasks.forEach((taskMock) => this._renderTask(taskMock));
+        this._taskListController.setTasks(sortedByDateUpTasks);
         break;
       case `date-down`:
         const sortedByDateDownTasks = this._tasks.slice().sort((a, b) => b.dueDate - a.dueDate);
-        sortedByDateDownTasks.forEach((taskMock) => this._renderTask(taskMock));
+        this._taskListController.setTasks(sortedByDateDownTasks);
         break;
       case `default`:
-        this._tasks.forEach((taskMock) => this._renderTask(taskMock));
+        this._taskListController.setTasks(this._tasks);
         break;
     }
   }
--- src/main.js
+++ src/main.js
@@ -1,32 +1,36 @@
-import {menu} from './components/menu.js';
-import {search} from './components/search.js';
-import {makeFilter} from './components/make-filter.js';
-import {getTask, getFilter} from './data/data.js';
+import {SiteMenu} from './components/site-menu.js';
+import {Search} from './components/search.js';
+import {Statistics} from './components/statistics.js';
+import {Filter} from './components/filter.js';
+import {getTask} from './data/data.js';
 import {BoardController} from './controllers/board.js';
+import {SearchController} from './controllers/search.js';
+import {render, Position} from './utils.js';
 
 const TASK_COUNT = 3;
 
 const main = document.querySelector(`.main`);
 const menuWrapper = document.querySelector(`.main__control`);
-const boardWrapper = document.createElement(`section`);
-boardWrapper.className = `board container`;
-const boardTasks = document.createElement(`div`);
-boardTasks.className = `board__tasks`;
 const textNoTasks = `<p class="board__no-tasks">Congratulations, all tasks were completed! To create a new click on«add new task» button.</p>`;
-
-const renderComponent = (wrapper, html) => {
-  wrapper.insertAdjacentHTML(`beforeend`, html);
-};
-
-renderComponent(menuWrapper, menu());
-renderComponent(main, search());
-renderComponent(main, makeFilter(getFilter()));
-main.append(boardWrapper);
-boardWrapper.append(boardTasks);
-
-const taskMocks = new Array(TASK_COUNT)
+const statistics = new Statistics();
+const siteMenu = new SiteMenu();
+const search = new Search();
+const filter = new Filter();
+let taskMocks = new Array(TASK_COUNT)
 .fill(``)
 .map(getTask);
+
+const onDataChange = (tasks) => {
+  taskMocks = tasks;
+};
+
+statistics.getElement().classList.add(`visually-hidden`);
+
+render(menuWrapper, siteMenu.getElement(), Position.BEFOREEND);
+render(main, menuWrapper, Position.BEFOREEND);
+render(main, search.getElement(), Position.BEFOREEND);
+render(main, filter.getElement(), Position.BEFOREEND);
+render(main, statistics.getElement(), Position.BEFOREEND);
 
 function isArchiveTasksArray() {
   taskMocks.every((it) => {
@@ -35,9 +39,55 @@ function isArchiveTasksArray() {
 }
 
 if (taskMocks.length === 0 || isArchiveTasksArray()) {
-  renderComponent(boardWrapper, textNoTasks);
+  render(main, textNoTasks);
 }
 
-const boardController = new BoardController(boardTasks, taskMocks);
-boardController.init();
+const taskListController = new BoardController(main, onDataChange);
+
+const onSearchBackButtonClick = () => {
+  statistics.getElement().classList.add(`visually-hidden`);
+  searchController.hide();
+  taskListController.show(taskMocks);
+};
+const searchController = new SearchController(main, search, onSearchBackButtonClick);
+
+taskListController.show(taskMocks);
+
+
+siteMenu.getElement().addEventListener(`change`, (evt) => {
+  evt.preventDefault();
+
+  if (evt.target.tagName !== `INPUT`) {
+    return;
+  }
+
+  if (evt.target.tagName !== `INPUT`) {
+    return;
+  }
+
+  const tasksId = `control__task`;
+  const statisticId = `control__statistic`;
+  const newTaskId = `control__new-task`;
+
+  switch (evt.target.id) {
+    case tasksId:
+      statistics.getElement().classList.add(`visually-hidden`);
+      taskListController.show(taskMocks);
+      break;
+    case statisticId:
+      taskListController.hide();
+      statistics.getElement().classList.remove(`visually-hidden`);
+      break;
+    case newTaskId:
+      taskListController.createTask();
+      siteMenu.getElement().querySelector(`#${tasksId}`).checked = true;
+      break;
+  }
+});
+
+search.getElement().addEventListener(`click`, () => {
+  statistics.getElement().classList.add(`visually-hidden`);
+  taskListController.hide();
+  searchController.show(taskMocks);
+});
 
